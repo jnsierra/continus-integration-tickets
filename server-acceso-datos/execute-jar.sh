8b18d0a0a39948bc4618fee887583361a9c1c6bd
jesus <jnsierrac@vps.server.local>
2023-03-15 16:36:48 +0000

    Ajuste para el stage

--- server-acceso-datos/execute-jar.sh
+++ server-acceso-datos/execute-jar.sh
@@ -1,3 +1,3 @@
-sh wait-for-postgres.sh $SERVER_BD
+#sh wait-for-postgres.sh $SERVER_BD
 sh wait-for-discovery.sh $SERVER_DISCOVERY
 java -jar -Dspring.profiles.active=$PROFILE_JAR api-acceso-datos-0.0.1-SNAPSHOT.jar
